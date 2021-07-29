4fdf78071f5c2200ade26cd4236f6bcaf5a9f467
ThiagoLuzNasc <thiagoifnmgsi2015@gmail.com>
2021-07-29 17:11:58 -0300

    Relacionamento entre classes UEC

---
+++ classeLutador.php
@@ -0,0 +1,28 @@
+<?php
+class Lutador{
+    //atributos
+    private $nome;
+    private $nacionalidade;
+    private $idade;
+    private $altura;
+    private $peso;
+    private $categoria;
+    private $vitorias;
+    private $derrotas;
+    private $empates;
+}
+public function apresentar(){
+
+}
+public function status(){
+
+}
+public function ganharLuta(){
+
+}
+public function perderLuta(){
+
+}
+public function empatarLuta(){
+    
+}
